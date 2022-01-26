2194167fda5271c2eb3f80925658c33110268b26
aleksandraskrzypek <79858293+aleksandraskrzypek@users.noreply.github.com>
2022-01-26 13:49:51 +0100

    add omnie

---
+++ components/Omnie.js
@@ -0,0 +1,35 @@
+function Omnie() {
+    return (
+        <div id="aboutme" className="grid grid-cols-1 md:grid-cols-2 gap-10 bg-black ">
+            <div className="mx-20 ml-20 md:mr-10 my-20 flex text-center border border-none bg-slate-100 bg-opacity-10 rounded-md">
+                <p className="text-white text-2xl p-2">
+                Dzień dobry, mam na imię Ola. Jestem osobą kreatywną i odpowiedzialną, szybko sie uczę, a nowe wyzwania są dla mnie najlepszą lekcją do samoudoskonalania się. Interesuję się frontendem - w ostatnim czasie najwięcej swojego czasu poświęcam na naukę React’a. Poniżej można zobaczyć wybrane projekty.
+                </p>
+            </div>
+
+            <div className="flex flex-wrap items-center justify-center gap-3 mr-20 my-20 md:ml-10 mx-20">
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">HTML</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">CSS</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">JavaScript</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">React</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Git</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Illustrator</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Gimp</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Next.js</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Tailwind</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Rest Api</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Responsive Web Design</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Komunikatywność</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Zaangażowanie</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Język Angielski</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Kreatywność</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Analityczne Myślenie</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Logiczne Myślenie</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Kultura Osobista</p>
+                <p className="border rounded-md border-none p-1 text-white bg-slate-100 bg-opacity-10 text-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110">Dobre znoszenie krytyki</p>
+            </div>
+        </div>
+    )
+}
+
+export default Omnie
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Intro from '../components/Intro'
+import Omnie from '../components/Omnie'
 
 export default function Home() {
   return (
     <div>
       <Intro />
+      <Omnie />
     </div>
   )
 }
